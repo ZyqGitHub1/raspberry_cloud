fbf10cdac4f2e213286f720abb669559ee45843f
unknown <>
2016-04-03 18:26:39 +0800

    更新前端文件->登录页

---
+++ app/static/js/index.js
@@ -0,0 +1,25 @@
+jQuery.fn.shake = function(intShakes, intDistance, intDuration) {
+    this.each(function() {
+        $(this).css({
+            position: 'relative',
+        });
+        for (var i = 1; i <= intShakes; i++) {
+            $(this).animate({
+                left: (intDistance * -1)
+            }, (((intDuration / intShakes) / 4))).animate({
+                left: intDistance
+            }, ((intDuration / intShakes) / 2)).animate({
+                left: 0
+            }, (((intDuration / intShakes) / 4)));
+        }
+    });
+    return this;
+};
+
+$("#signin").click(function(){
+	$("input.form-control").shake(4,20,200)
+})
+
+$("#register").click(function(){
+	$("#register-window").modal('show')
+})
