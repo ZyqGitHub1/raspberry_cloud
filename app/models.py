c39aa35dccc455103840f00181a81c472e2d572d
chenghao <940068139@qq.com>
2016-04-10 00:36:30 +0800

    4/10/0:36

--- app/models.py
+++ app/models.py
@@ -1,9 +1,10 @@
 from werkzeug.security import check_password_hash, generate_password_hash
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
-from flask import current_app
+from flask import current_app, request
 from . import db
 from . import login_manager
 from flask.ext.login import UserMixin, AnonymousUserMixin
+import hashlib
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -92,7 +93,14 @@ class User(UserMixin, db.Model):
         db.session.add(self)
         return True
 
-
+    def gravatar(self, size=100, default='identicon', rating='g'):
+        if request.is_secure:
+            url = 'https://secure.gravatar.com/avatar'
+        else:
+            url = 'http://www.gravatar.com/avatar'
+        hash = hashlib.md5(self.email.encode('utf-8')).hexdigest()
+        return '{url}/{hash}?s={size}&d={default}&r={rating}'.format(
+            url=url, hash=hash, size=size, default=default, rating=rating)
 
     def __repr__(self):
         return '<User %r>' % self.username
