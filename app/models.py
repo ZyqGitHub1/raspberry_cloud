5f4147d11898783ea185e8091d887bec7fabf8e1
chenghao <940068139@qq.com>
2016-04-10 22:35:17 +0800

    4/10/22:35

--- app/models.py
+++ app/models.py
@@ -93,15 +93,6 @@ class User(UserMixin, db.Model):
         db.session.add(self)
         return True
 
-    def gravatar(self, size=100, default='identicon', rating='g'):
-        if request.is_secure:
-            url = 'https://secure.gravatar.com/avatar'
-        else:
-            url = 'http://www.gravatar.com/avatar'
-        hash = hashlib.md5(self.email.encode('utf-8')).hexdigest()
-        return '{url}/{hash}?s={size}&d={default}&r={rating}'.format(
-            url=url, hash=hash, size=size, default=default, rating=rating)
-
     def __repr__(self):
         return '<User %r>' % self.username
 
