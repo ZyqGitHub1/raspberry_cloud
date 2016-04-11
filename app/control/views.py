26f76af7ba48f4db49ed6556b402db2fe593fc99
chenghao <940068139@qq.com>
2016-04-12 01:46:20 +0800

    添加电器

--- app/control/views.py
+++ app/control/views.py
@@ -1,6 +1,6 @@
 from flask import render_template,redirect,request,url_for,flash,jsonify,json
 from flask.ext.login import login_user,logout_user,login_required
-from .. models import User
+from .. models import *
 from . import control
 from .. import db
 
@@ -9,18 +9,45 @@ def noneIfEmptyString(value):
 		return None
 	return value
 
-@control.route('/out',methods=['GET', 'POST'])
+# @control.route('/out',methods=['GET', 'POST'])
+# @login_required
+# def out():
+# 	data = request.form
+# 	print data
+# 	pin_number = data.get('pin_number')
+# 	id = int(pin_number)
+# 	GPIO.setmode(GPIO.BCM)
+# 	if request.form['turn'] == "on":
+# 		GPIO.setup(id,GPIO.OUT)
+# 		GPIO.output(id,True)
+# 	if request.form['turn'] == "off":
+# 		GPIO.setup(id,GPIO.OUT)
+# 		GPIO.output(id,False)
+
+@control.route('/add_electrical', methods=['GET', 'POST'])
 @login_required
-def out():
+def add_electricals():
 	data = request.form
 	print data
-	pin_number = data.get('pin_number')
-	id = int(pin_number)
-	GPIO.setmode(GPIO.BOARD)
-	if request.form['turn'] == "on":
-		GPIO.setup(id,GPIO.OUT)
-		GPIO.output(id,True)
-	if request.form['turn'] == "off":
-		GPIO.setup(id,GPIO.OUT)
-		GPIO.output(id,False)
-
+	electrical_name = noneIfEmptyString(data.get('electrical_name'))
+	remask = noneIfEmptyString(data.get('remask'))
+	status = noneIfEmptyString(data.get('status'))
+	try:
+		electrical = Electrical(electrical_name=electrical_name,
+								pin_id=Pin.query_by(useable=True).first().bcm_id,
+								remark=remark)
+		db.session.add(electrical)
+		result = {
+		'successful': True
+		}
+	except:
+		if(electrical_name==None):
+			result = {
+			'error': 0
+			}
+		else:
+			result = {
+			'error': 1
+			}
+	finally:
+		return jsonify(result)
